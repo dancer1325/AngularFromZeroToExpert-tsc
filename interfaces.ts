59b65d0a6a8edcfb270d20aae77c2a7bdd370798
dancer1325 <alfredotic0809@gmail.com>
2023-04-25 16:58:29 +0200

    refactor(intertfaces.ts): adjust whiteline spacing (Section 2)

--- interfaces.ts
+++ interfaces.ts
@@ -9,7 +9,6 @@ let Wolverine = {
   nombre: "wolverine",
   poder: "Regeneracion",
 };
-
 enviarMision(Wolverine);
 
 //Si cambio el atributo para que no se llame nombre, dará un nanDefined cuando se invoque a la función enviarMision.
@@ -26,7 +25,6 @@ function enviarMisionSpecifingThePropertyInTheArgument(xmen: {
   //En el momento en el que estoy xmen.: como no se ha definido nombre para el objeto "xmen", no me da ninguna sugerencia
   console.log("Enviando a: " + xmen.nombre);
 }
-
 enviarMisionSpecifingThePropertyInTheArgument(Wolverine);
 
 // Defining an interface
